43c91549a5f1664be94a507aae8b6c8f74905c35
Gokul01Elango <gokulelango1040@gmail.com>
2025-12-28 09:22:08 +0530

    Day-1-count-digit.py

---
+++ python-dsa-playground/maths/day1_count_digit.py
@@ -0,0 +1,22 @@
+
+
+# value = 221
+# b = 23   output: 2
+
+# Count the value - loop the number and find count
+
+
+# 234/10 = > 23 /10 => 2/10 => 0
+
+a=int(input("enter your number"))
+count=0
+# a=1234
+# print(a//10)
+# -12232
+print(-12334/10)
+while a>0:
+    count=count+1
+    a=a//10
+    # print(a)
+    
+print(count)
